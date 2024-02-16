6f9ab2559338b71e9a1fae07296208071fd69580
PeteTheGameDev <pe897423@ucf.edu>
2024-02-16 10:35:15 -0500

    initial commit

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        this.playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        this.UpdateActions();
+    }
+
+    private void UpdateActions()
+    {
+        this.movement = this.playerInput.actions["Move"].ReadValue<Vector2>().normalized;
+        this.left = this.movement.x < 0;
+        this.right = this.movement.x > 0;
+
+        this.firePressed = this.playerInput.actions["Fire"].WasPressedThisFrame();
+        
+        this.restartPressed = this.playerInput.actions["Restart"].WasPressedThisFrame();
+    }
+
+    // Input Actions Asset
+    private PlayerInput playerInput;
+
+    // Move "WASD" & Left Stick
+    public Vector2 movement {get; private set;}
+    public bool left {get; private set;}
+    public bool right {get; private set;}
+
+    // Fire
+    public bool firePressed {get; private set;}
+
+    // Restart
+    public bool restartPressed {get; private set;}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour
+{
+    private InputManager input;
+
+    public float speed;
+
+    public Projectile laserPrefab;
+
+    private Projectile laser;
+
+    public void Start()
+    {
+        input = GetComponentInChildren<InputManager>();
+    }
+
+    public void Update()
+    {
+        Movement();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (input.firePressed)
+        {
+            if (laser == null) 
+            {
+                laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            }
+        }
+    }
+
+    private void Movement()
+    {
+        Vector3 position = transform.position;
+
+        // Input
+        if (input.left)
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+        else if (input.right)
+        {
+            position.x += speed * Time.deltaTime;
+        }
+
+        // Clamp
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+
+        // New position
+        transform.position = position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Missile") ||
+            other.gameObject.layer == LayerMask.NameToLayer("Invader")) {
+            GameManager.Instance.OnPlayerKilled(this);
+        }
+    }
+}
